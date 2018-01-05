66ae5b2077db259a5a1a0d6221a74c3ae4aafffe
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-01-05 15:57:48 -0600

    Added Data as a Datable

---
+++ Sources/Datable/Data+Datable.swift
@@ -0,0 +1,14 @@
+//
+//  Data+Datable.swift
+//  DatablePackageDescription
+//
+//  Created by Brandon Wiley on 1/5/18.
+//
+
+import Foundation
+
+extension Data: Datable {
+    public var data: Data {
+        return self
+    }
+}
