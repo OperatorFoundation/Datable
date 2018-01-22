e30cbdd1a9a7264fcbb3ebfe415a24420ae7ac13
Lita <looklita@gmail.com>
2018-01-22 13:07:21 -0600

    Support for Double and Float

---
+++ Sources/Datable/Datable+Double.swift
@@ -0,0 +1,54 @@
+//
+//  Datable+Double.swift
+//  Datable
+//
+//  Created by Adelita Schule on 1/22/18.
+//
+
+import Foundation
+
+extension Double: Datable
+{
+    public init(data: Data)
+    {
+        var value = data.withUnsafeBytes
+        {
+            (ptr: UnsafePointer<Float>) -> Float in
+            return ptr.pointee
+        }
+        
+        if DatableConfig.endianess != DatableConfig.localEndianness
+        {
+            switch DatableConfig.endianess
+            {
+            case .little:
+                value = Float(bitPattern: UInt32(littleEndian: data.withUnsafeBytes {$0.pointee}))
+            case .big:
+                value = Float(bitPattern: UInt32(bigEndian: data.withUnsafeBytes {$0.pointee}))
+            }
+        }
+        
+        self.init(value)
+    }
+    
+    public var data: Data
+    {
+        get
+        {
+            var value = self.bitPattern
+            
+            if DatableConfig.endianess != DatableConfig.localEndianness
+            {
+                switch DatableConfig.localEndianness
+                {
+                case .little:
+                    value = value.bigEndian
+                case .big:
+                    value = value.littleEndian
+                }
+            }
+            
+            return Data(buffer: UnsafeBufferPointer(start: &value, count: 1))
+        }
+    }
+}
