ba02090b4fae616a6c5ad43c1af2a36bfcef3054
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-07-30 12:38:59 -0500

    Added Intable for String/Int conversions

---
+++ Sources/Datable/Intable.swift
@@ -0,0 +1,40 @@
+//
+//  Intable.swift
+//  DatablePackageDescription
+//
+//  Created by Brandon Wiley on 7/30/18.
+//
+
+import Foundation
+
+public protocol Intable {
+    init(int: Int)
+    var int: Int { get }
+    
+    init(int8: Int8)
+    var int8: Int8 { get }
+    
+    init(int16: Int16)
+    var int16: Int16 { get }
+    
+    init(int32: Int32)
+    var int32: Int32 { get }
+    
+    init(int64: Int64)
+    var int64: Int64 { get }
+    
+    init(uint: UInt)
+    var uint: UInt { get }
+    
+    init(uint8: UInt8)
+    var uint8: UInt8 { get }
+    
+    init(uint16: UInt16)
+    var uint16: UInt16 { get }
+    
+    init(uint32: UInt32)
+    var uint32: UInt32 { get }
+    
+    init(uint64: UInt64)
+    var uint64: UInt64 { get }
+}
---
+++ Sources/Datable/String+Intable.swift
@@ -0,0 +1,80 @@
+//
+//  String+Intable.swift
+//  DatablePackageDescription
+//
+//  Created by Brandon Wiley on 7/30/18.
+//
+
+import Foundation
+
+extension String: Intable {
+    public init(int: Int) {
+        return String(int)!
+    }
+    public var int: Int {
+        return Int(self)!
+    }
+    
+    public init(int8: Int8) {
+        return String(int)!
+    }
+    public var int8: Int8 {
+        return Int8(self)!
+    }
+    
+    public init(int16: Int16) {
+        return String(int16)!
+    }
+    public var int16: Int16 {
+        return Int16(self)!
+    }
+    
+    public init(int32: Int32) {
+        return String(int32)!
+    }
+    public var int32: Int32 {
+        return Int32(self)!
+    }
+    
+    public init(int64: Int64) {
+        return String(int64)!
+    }
+    public var int64: Int64 {
+        return Int16(self)!
+    }
+    
+    public init(uint: UInt) {
+        return String(uint)!
+    }
+    public var uint: UInt {
+        return UInt(self)!
+    }
+    
+    public init(uint8: UInt8) {
+        return String(uint8)!
+    }
+    public var uint8: UInt8 {
+        return UInt8(self)!
+    }
+    
+    public init(uint16: UInt16) {
+        return String(uint16)!
+    }
+    public var uint16: UInt16 {
+        return UInt16(self)!
+    }
+    
+    public init(uint32: UInt32) {
+        return String(uint32)!
+    }
+    public var uint32: UInt32 {
+        return UInt32(self)!
+    }
+    
+    public init(uint64: UInt64) {
+        return String(uint64)!
+    }
+    public var uint64: UInt64 {
+        return UInt64(self)!
+    }
+}
