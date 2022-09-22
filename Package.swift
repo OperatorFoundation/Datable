2a40ccc949fdf71d51ee43b287954dd8b8f26ce9
Unseelie <emunseelie@gmail.com>
2022-09-22 16:15:11 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Datable",
-    platforms: [.macOS(.v10_15), .iOS(.v15)],
+    platforms: [.macOS(.v10_10), .iOS(.v15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
