96476de048991cd6cb9ceb0d0d26b889767e2e7d
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2023-07-19 16:43:30 -0500

    changed platform to .macOS(.v12)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Datable",
 
-    platforms: [.macOS(.v13), .iOS(.v15)],
+    platforms: [.macOS(.v12), .iOS(.v9)],
+
 
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
