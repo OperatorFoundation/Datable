2bd089c0688434a9246ab4b4d4c814acaf4415ea
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2023-07-19 15:18:15 -0500

    Updated swiftVersion = 5.8 and platforms .macOS13 & .iOS15

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Datable",
-    platforms: [.macOS(.v10_10), .iOS(.v9)],
+    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
