891f6c88938f366055ddb6be8fb2a602ea868f97
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2023-07-19 16:37:34 -0500

    Updated iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Datable",
 
-    platforms: [.macOS(.v13), .iOS(.v9)],
+    platforms: [.macOS(.v13), .iOS(.v15)],
 
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
